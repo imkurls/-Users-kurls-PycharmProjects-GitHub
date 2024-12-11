a15c35fb804cbe6ba409b3b800bbe097922bfbd9
imkurls <imkurls@gmail.com>
2024-12-11 15:54:09 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,29 @@
+# "Организация программ и методы строк" 08.12.
+my_string=input('как дела')
+
+print (len(my_string))
+print (my_string.upper())
+print (my_string.upper().lower())
+print (my_string.replace(' ',''))
+print (my_string[0])
+print (my_string[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ module_1_5.py
@@ -0,0 +1,43 @@
+#практическое задание изменяемые и неизменяемые объеты, кортежи
+name=('immutable_var')
+tuple_ =name,5,2,3,
+print(tuple_)
+tuple_1 =name,1,2,3,'-'
+print(tuple_1)
+#
+name_1=('mutable_var:')
+list=[name_1,1,2,'a','b']
+print(list)
+print(list[0:4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
