cf99703cee71ed07970540b8e8d40d941c7b1d2e
imkurls <imkurls@gmail.com>
2024-12-12 12:26:38 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,21 @@
+my_dict={'Kostya-':1970,'Vasyia-':1950,'Sasha-':1964}
+print(my_dict)
+print(my_dict['Kostya-'])
+my_dict['Ira']=2000
+print(my_dict)
+my_dict.update({'Luda':1965,'Misha':1939})
+print(my_dict)
+del my_dict['Misha']
+print(my_dict.get('Misha',1939))
+my_set={1,2,3,4,5,4,2,'string'}
+print(my_set)
+print(my_set.add(6))
+print(my_set)
+print(my_set.add(7))
+print(my_set)
+print(my_set.remove(7))
+print(my_set)
+
+
+
+
