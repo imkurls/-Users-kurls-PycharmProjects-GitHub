c1b48ddccf09955937f36a125413d465b9567116
imkurls <imkurls@gmail.com>
2024-12-13 14:50:41 +0300

    Add files via upload

---
+++ SUM_mod_1.py
@@ -0,0 +1,37 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+Aaron_res=(grades[0])
+Aaron_mid_res=('Aaron средний балл:',(5+3+3+5+4)/5)
+print('Оценки Aaron:',Aaron_res)
+print(Aaron_mid_res)
+Bilbo_res=(grades[1])
+Bilbo_mid_res=('Bilbo средний балл:',round(2+2+2+3)/4)
+print('Оценки Bilbo:',Bilbo_res)
+print (Bilbo_mid_res)
+Johnny_res=(grades[2])
+Johnny_mid_res=('Johnny средний балл:',(4+5+5+2)/4)
+print('Оценки Johnny:',Johnny_res)
+print (Johnny_mid_res)
+Khendrik_res=(grades[3])
+Khendrik_mid_res=('Khendrik средний балл:',((4+4+3)/3))
+print('Оценки Khendrik:',Khendrik_res)
+print (Khendrik_mid_res)
+Steve_res=grades[4]
+Steve_mid_res=('Steve средний балл:',(5+5+5+4+5)/5)
+print('оценки Steve:',Steve_res)
+print(Steve_mid_res)
+
+
+
+
+#data=( [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]])
+#print(str(data)[1:-1])
+#test_=(grades[0])=(5+5+5+4+5)
+#print(test_)
+
+
+
+
+
+
+
