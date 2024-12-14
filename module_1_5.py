5e89ebeaf4c66b04c29daa347b5821228c50c06e
imkurls <imkurls@gmail.com>
2024-12-14 15:48:38 +0300

    Add files via upload

--- module_1_5.py
+++ module_1_5.py
@@ -2,10 +2,10 @@
 name=('immutable_var')
 tuple_ =name,5,2,3,
 print(tuple_)
-#tiple_[0]=10
-#print(tuple_)
 tuple_1 =name,1,2,3,'-'
 print(tuple_1)
+#tiple_[0]=10
+#print(tuple_)
 #
 name_1=('mutable_list:')
 list=[name_1,1,2,'a','b']
