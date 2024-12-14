e0af77f62239a9c6eb96640879958549bf80b1ed
imkurls <imkurls@gmail.com>
2024-12-14 11:40:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-# Home task "переменные" 06.12.
-home_tasks_execute=(12)
-time_execute=(1.5)
-mte=(home_tasks_execute/time_execute)
-course_name=('"Python"')
-name=('количество домашних заданий:')
-newname=('время выполнения домашних заданий:')
-midname=('среднее время домашних заданий:')
-print(course_name, name, home_tasks_execute,newname,time_execute, midname, mte)
-
-
-
-
-
-
-
-
-
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Домашнее задание "строки и индексация строк" 07.12.
+example= 'Kurls'
+print(example[0])
+print(example[-1])
+print(example[2:])
+print(example[::-1])
+print(example[1:4:2])
