d09210faa6efd898eff743d3229ecbe67b46c099
imkurls <imkurls@gmail.com>
2024-12-14 11:21:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-#"1st program"
-print (9**0.5*5)
-#"2nd program"
-print (9.99>9.98, 1000!=1000.1)
-#"3rd program"
-print(2*2+2)
-print (2*(2+2))
-print (2*2+2==2*(2+2))
-#"4th program"
-print(int(123.456-119.056))# c предложенным подходом (*10, int и остаточным делением нет решения)
+# Home task "переменные" 06.12.
+home_tasks_execute=(12)
+time_execute=(1.5)
+mte=(home_tasks_execute/time_execute)
+course_name=('"Python"')
+name=('количество домашних заданий:')
+newname=('время выполнения домашних заданий:')
+midname=('среднее время домашних заданий:')
+print(course_name, name, home_tasks_execute,newname,time_execute, midname, mte)
+
+
+
+
+
+
+
+
+
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
