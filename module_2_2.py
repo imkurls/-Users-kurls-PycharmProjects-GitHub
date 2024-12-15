3e966b0a317e82f30b887900c44818d0144b789b
imkurls <imkurls@gmail.com>
2024-12-15 15:17:15 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,22 @@
+first=4
+second=5
+third=6
+if first==second==third:
+    print(3)
+if first==second or first==third or second==third:
+    print(2)
+else:
+    print(0)
+
+
+
+
+
+
+
+
+
+
+    
+
+
