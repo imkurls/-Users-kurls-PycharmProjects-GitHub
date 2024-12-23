79244ee924f5c1368b1304bbcd7faa9e0141a7b7
imkurls <imkurls@gmail.com>
2024-12-23 10:22:15 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+#Домашняя работа по уроку "Стиль кода часть II. Цикл While"
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i=0
+while my_list[i] >= 0 and i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    i= i + 1
+
+    
